96ded2eb672440799f2f5183d99713f2b0b246e8
Mateus Pereira <mateusmichellipereira@gmail.com>
2023-12-17 16:43:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const url = 'https://dummyjson.com/user/2';
-const paragrafoElemento = document.createElement('p');
-document.body.appendChild(paragrafoElemento);
-async function consumirURL() {
-    try {
-        const resposta = await fetch(url);
-        const dados = await resposta.json();
-        paragrafoElemento.textContent = JSON.stringify(dados);
-    } catch (erro) {
-        console.error('Erro ao consumir a URL:', erro);
+(function() {
+    const url = 'https://dummyjson.com/user/2';
+    const paragrafoElemento = document.createElement('p');
+    document.body.appendChild(paragrafoElemento);
+
+    async function consumirURL() {
+        try {
+            const resposta = await fetch(url);
+            const dados = await resposta.json();
+            paragrafoElemento.textContent = JSON.stringify(dados);
+        } catch (erro) {
+            console.error('Erro ao consumir a URL:', erro);
+        }
     }
-}
-consumirURL();
+
+    consumirURL();
+})();
